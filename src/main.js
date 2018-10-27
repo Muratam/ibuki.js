b43a65e2fda1f862b40fc5c2812f781a33966c24
murata <murata@kmc.gr.jp>
2018-10-27 21:45:55 +0900

    for class

--- src/main.js
+++ src/main.js
@@ -6,20 +6,42 @@ function appendStyle(code) {
   document.head.appendChild(style);
 }
 
-function create(tag = "div", style = {}, options = {}) {
-  let elem = document.createElement(tag);
-  for (let key in style) elem.style[key] = style[key];
-  for (let key in options) elem[key] = options[key];
-  return elem;
+function applyStyle(elem, style = {}) {
+  const withUnit = x => (typeof (x) === "number" ? `${x}px` : x);
+  for (let key in style) {
+    let val = style[key];
+    if (typeof (val) === "object") {
+      let prefix = key; // 1階層まで省略できるように
+      for (let kk in val) {
+        let currentKey = kk === "" ? prefix : prefix + "-" + kk;
+        elem.style[currentKey] = withUnit(val[kk]);
+      }
+    } else {
+      elem.style[key] = withUnit(val);
+    }
+  }
 }
 
-function repeat(fun) {
+
+function update(fun) {
   // WARN: 流用したほうが良い?(複数のrequestAnimationFrameは遅い?)
-  let repeatFun = () => {
-    fun();
-    requestAnimationFrame(repeatFun);
+  let updateFun = () => {
+    let ok = fun();
+    if (ok !== false) requestAnimationFrame(updateFun);
   };
-  requestAnimationFrame(repeatFun);
+  updateFun();
+}
+
+function create(tag = "div", style = {}, options = {}, updateFunction = undefined) {
+  let elem = document.createElement(tag);
+  applyStyle(elem, style)
+  for (let key in options) {
+    if (key === "parent") options[key].appendChild(elem);
+    else if (key === "update") update(options[key].bind(elem))
+    else if (key !== "awake") elem[key] = options[key];
+  }
+  if (options["awake"]) options["awake"].bind(elem)();
+  return elem;
 }
 
 if (true) {
@@ -27,71 +49,104 @@ if (true) {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
   }`);
-  let imgs = [];
 
-  function createImg() {
+  class TestImage {
+    constructor() {
+      const square = 400;
+      this.x = Math.floor(square * Math.random()) % square;
+      this.y = Math.floor(square * Math.random()) % square;
+    }
+    get initialStyle() {
+      return {
+        position: "absolute",
+        color: "#f00000",
+        top: this.x,
+        left: this.y,
+        animation: {
+          "": "rot 1s",
+          "iteration-count": `infinite`,
+        },
+        border: {
+          radius: 20,
+          width: 3,
+          style: "solid",
+          color: "#000000",
+        }
+      };
+    }
+    get initialAttribute() {
+      return {
+        width: 100,
+        src: "./img/img.png",
+        parent: parent,
+      };
+    }
+    update() {
+      this.x++;
+      this.y++;
+      applyStyle(this, {
+        top: this.x,
+        left: this.y,
+        transform: `rotate(${this.x + this.y}deg)`,
+        "border-radius": this.x % 100
+      });
+      if (this.x > 500) {
+        this.remove();
+        return false;
+      }
+    }
+  }
+
+  function createImg(parent = document.body) {
     const square = 400;
     let x = Math.floor(square * Math.random()) % square;
     let y = Math.floor(square * Math.random()) % square;
-    let img = create("img", {
-      animation: "rot 1s",
+    return create("img", {
       position: "absolute",
       color: "#f00000",
-      // "animation-iteration-count": `infinite`,
-      top: `${x}px`,
-      left: `${y}px`,
-      "border-radius": "20px",
+      top: x,
+      left: y,
+      animation: {
+        "": "rot 1s",
+        "iteration-count": `infinite`,
+      },
+      border: {
+        radius: 20,
+        width: 3,
+        style: "solid",
+        color: "#000000",
+      }
     }, {
       width: 100,
       src: "./img/img.png",
-    });
-    // repeat(() => {
-    //   img.style.transform = `rotate(${x + y}deg)`;
-    //   x++;
-    //   y++;
-    //   img.style.top = `${x}px`;
-    //   img.style.left = `${y}px`;
-    // });
-    return [img, {
-      dom: img,
-      x: x,
-      y: y,
-      isValid: true
-    }];
-  }
-
-  function appendImg(parent = document.body) {
-    let [img, data] = createImg();
-    parent.appendChild(img);
-    for (let i = 0; i < imgs.length; i++) {
-      if (imgs[i].isValid) continue;
-      imgs[i] = data;
-      return;
-    }
-    imgs.push(data);
-  }
-  appendImg();
-  let i = 0;
-  repeat(() => {
-    for (let img of imgs) {
-      if (!img.isValid) continue;
-      img.dom.style.transform = `rotate(${img.x + img.y}deg)`;
-      img.x++;
-      img.y++;
-      img.dom.style.top = `${img.x}px`;
-      img.dom.style.left = `${img.y}px`;
-      if (img.x > 500) {
-        img.dom.remove();
-        img.isValid = false;
+      parent: parent,
+      awake() {},
+      update() {
+        x++;
+        y++;
+        applyStyle(this, {
+          top: x,
+          left: y,
+          transform: `rotate(${x + y}deg)`,
+          "border-radius": x % 100
+        });
+        if (x > 500) {
+          this.remove();
+          return false;
+        }
       }
-    }
+    });
+  }
+  createImg();
+  let i = 0;
+  update(() => {
     i += 1;
-    if (i % 10 == 0) appendImg();
+    if (i % 10 == 0) createImg();
   });
 }
 
 if (true) {
-  repeat(() => {
+  update(() => {
     let globalWidth = window.innerWidth;
     let globalHeight = screen.height;
     // console.log(globalWidth);
